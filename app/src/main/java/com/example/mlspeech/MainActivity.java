5a9e12cdb6d0b52d6a738b4831b7c7363546c213
Cody Peterson <CGPeterson@student.fullsail.edu>
2021-06-06 12:36:29 -0600

    Restarted and got the recording working

--- app/src/main/java/com/example/mlspeech/MainActivity.java
+++ app/src/main/java/com/example/mlspeech/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.mlspeech;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.tvacstudio.audiorecorder.R;
+
+import java.io.File;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
 }
